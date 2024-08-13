241848922cb8208bdf73eba096ec2ba49f8b40c8
Richard Ly <rly@nextlevelems.com>
2024-08-13 10:52:30 -0700

    fix deployment issues

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const saltRounds = 10;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
